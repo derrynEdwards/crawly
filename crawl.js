f5694030b42e4c86c66b213acaeefec79dc3a729
Derryn Edwards <derryn_93@live.com>
2023-08-18 16:02:57 -0600

    adds getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,7 +1,32 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const aElements = dom.window.document.querySelectorAll('a');
+
+    for (const aElement of aElements) {
+        if (aElement.href.slice(0, 1) === '/') {
+            try {
+                urls.push(new URL(aElement.href, baseURL).href);
+            } catch (err) {
+                console.log(`${err.message}: ${aElement.href}`)
+            };
+        } else {
+            try {
+                urls.push(new URL(aElement.href).href);
+            } catch (err) {
+                console.log(`${err.message}: ${aElement.href}`);
+            };
+        };
+    };
+    return urls;
+};
+
 function normalizeURL(url) {
     const urlObj = new URL(url);
     let normUrl = `${urlObj.host}${urlObj.pathname}`;
-    
+
     if (normUrl[normUrl.length - 1] === '/') {
         normUrl = normUrl.slice(0, normUrl.length - 1);
     };
@@ -13,8 +38,9 @@ function normalizeURL(url) {
     }
 
     return normUrl;
-}
+};
 
 module.exports = {
-    normalizeURL
-}
+    normalizeURL,
+    getURLsFromHTML
+};
